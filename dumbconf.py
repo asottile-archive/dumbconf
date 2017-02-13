efef43d957e2ecaf83e5df9089e498957db4db0d
Anthony Sottile <asottile@umich.edu>
2017-02-13 10:03:05 -0800

    Add loads function

--- dumbconf.py
+++ dumbconf.py
@@ -461,3 +461,26 @@ def debug(ast_obj, _indent=0):
                 out += '{}{}={},\n'.format(indentstr(), field, representation)
         out += indentstr() + ')'
         return out
+
+
+def _to_python_value(ast_obj):
+    if isinstance(ast_obj, ast.Doc):
+        return _to_python_value(ast_obj.body)
+    elif isinstance(
+            ast_obj, (ast.BareWordKey, ast.Bool, ast.Null, ast.String),
+    ):
+        return ast_obj.val
+    elif isinstance(ast_obj, ast.List):
+        return [_to_python_value(item.val) for item in ast_obj.items]
+    elif isinstance(ast_obj, ast.Map):
+        return collections.OrderedDict(
+            (_to_python_value(item.key), _to_python_value(item.val))
+            for item in ast_obj.items
+        )
+    else:
+        raise AssertionError('Unknown ast: {!r}'.format(ast_obj))
+
+
+def loads(s):
+    """Return a python object"""
+    return _to_python_value(parse(s))
--- tests/dumbconf_test.py
+++ tests/dumbconf_test.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import collections
+
 import pytest
 
 import dumbconf
@@ -638,3 +640,28 @@ def test_parse_error_token_expected():
         '1   |{True:,}\n'
         '           ^\n',
     )
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        ('True', True),
+        ('False', False),
+        ('None', None),
+        ("'ohai'", 'ohai'),
+    ),
+)
+def test_loads_simple(s, expected):
+    assert dumbconf.loads(s) == expected
+
+
+def test_loads_list():
+    src = '[True, False, "string"]'
+    assert dumbconf.loads(src) == [True, False, 'string']
+
+
+def test_loads_map():
+    src = "{a: 'a_value', b: 'b_value', c: 'c_value'}"
+    ret = dumbconf.loads(src)
+    assert isinstance(ret, collections.OrderedDict)
+    assert ret == {'a': 'a_value', 'b': 'b_value', 'c': 'c_value'}

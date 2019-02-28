9c40ee89e7846d2645912e8b3e73684e22503da5
Anthony Sottile <asottile@umich.edu>
2019-02-27 22:09:40 -0800

    Migrate setup.py to setup.cfg declarative metadata
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -1,21 +1,2 @@
-from setuptools import find_packages
 from setuptools import setup
-
-setup(
-    name='dumbconf',
-    description='A dumb configuration language.',
-    url='https://github.com/asottile/dumbconf',
-    version='0.0.0',
-    author='Anthony Sottile',
-    author_email='asottile@umich.edu',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-    packages=find_packages(exclude=('tests*', 'testing*')),
-)
+setup()
